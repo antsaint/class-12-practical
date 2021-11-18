6010c51c0206c71740d8ba71a4a7e59eabb80875
Siddhant Tiwari <antsaint@pm.me>
2021-11-18 07:57:05 +0530

    Finally started something

---
+++ Untitled-1.py
@@ -0,0 +1,19 @@
+#  import pandas
+# print("Twinkle, twinkle, little star")
+# print("How I wonder what you are")
+# print("Up above the world so high")
+# print("Like a diamond in the sky")
+# print("Twinkle, twinkle, little star")
+# print("How I wonder what you are")
+# print("Twinkle, twinkle, little star")
+# print("How I wonder what you are")
+# print("Up above the world so high")
+# print("Like a diamond in the sky")
+# print("Twinkle, twinkle, little star")
+# print("How I wonder what you are")
+import os
+print(os.listdir())
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hi Siddhant, I am a python module speaking to you.")
+engine.runAndWait()
