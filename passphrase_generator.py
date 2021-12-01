4521a55f478b4ae3cfe85d3da23a2acf14891bc8
Siddhant Tiwari <antsaint@pm.me>
2021-12-01 07:48:13 +0530

    V2

--- passphrase_generator.py
+++ passphrase_generator.py
@@ -1,21 +1,37 @@
-# We import sample so as to get desired number of random items from our list.
+from random import randint as rn
 from random import sample
 
-# Then we open the .txt file which contains large number of words
-wordlist_file = open("C:/Users/dell/Desktop/Python Practice/eff_large_wordlist.txt")
+def password_gen():
+    list1 = []
+    i = 0
+    desired_lenth = int(input("How many digits password you want buddy? "))
+    while i < desired_lenth:
+        new_num = rn(1, 9)
+        list1.append(str(new_num))
+        i += 1
+ 
+    password = ''.join(list1)
+    print(f'you password is {password}')
 
-# We spit out the words into a list
-words_list = wordlist_file.read().splitlines()
+def passphrase_gen():
+    wordlist_file = open("C:/Users/dell/Desktop/Python Practice/eff_large_wordlist.txt")
+    words_list = wordlist_file.read().splitlines()
+    desired_lenth = input("How many words do you need in passphrase? ")
+    password = sample(words_list, int(desired_lenth))
+    displayed_password = '-'.join(password)
+    print(f"Your passphrase is {displayed_password}")
 
-# We ask for desired length of passphrase
 
-desired_lenth = input("How many words do you need in passphrase? ")
+def main():
+    print("Do you want password or passphrase? ")
+    choice = input("Type Word for password and Phrase for passphrase: ")
+    if choice.upper() == "WORD":
+        password_gen()
+    elif choice.upper() == "PHRASE":
+        passphrase_gen()
+    else:
+        print("Type the given options only !")
+        main()
 
-# Finally using sample function we take desired number of random words from our list
-password = sample(words_list, int(desired_lenth))
 
-# We join the words so as to make password readable
-displayed_password = '-'.join(password)
-
-# Finally we print it.
-print(f"Your passphrase is {displayed_password}")
+main()
